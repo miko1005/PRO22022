082aaf3c40b777c16a9563ebcb1f32bd7961c80e
Alija Šerifović <alijaserifovic98@gmail.com>
2022-10-13 10:22:37 +0200

    a

---
+++ NiSignala/NiSignala/Kupec.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiSignala
+{
+    internal class Kupec
+    {
+        private string ime;
+        private decimal stanje;
+
+        public decimal Stanje { get => stanje; set => stanje = value; }
+        public string Ime { get => ime; set => ime = value; } //ne smes spreminjat izven kupec
+        public  void BeležiPlačilo(decimal plačilo)
+        {
+            stanje -= plačilo;
+        }
+        public void BeležiKlic(int min,TipKlica tip)
+        {
+            switch (tip)
+            {
+                case TipKlica.Stacionarno:
+                    stanje += min * 0.2m;
+                    break;
+                case TipKlica.Mobilno:
+                    stanje += min * 0.03m;
+                    break;
+                default:
+                    break;
+            }
+         
+        }
+    }
+}
---
+++ NiSignala/NiSignala/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiSignala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kupec Janez = new Kupec();
+            Janez.Ime = "Janez Novak";
+            Janez.BeležiKlic(10, TipKlica.Stacionarno);
+            Console.WriteLine(Janez.Ime + " dolguje " + Janez.Stanje);
+            Kupec Alenka=new Kupec();
+            Alenka.Ime = "Alenka Novak";
+            Alenka.BeležiKlic(10, TipKlica.Mobilno);
+            Console.WriteLine(Alenka.Ime + " dolguje "+ Alenka.Stanje);
+            Console.ReadLine();
+        }
+    }
+}
