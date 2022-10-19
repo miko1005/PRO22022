c70d5ab5cef489ffde93493cd598234ac2404ecb
Alija Šerifović <alijaserifovic98@gmail.com>
2022-10-19 08:58:02 +0200

    19.10

---
+++ Pravokotnik/Pravokotnik/pravokotnik.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pravokotnik
+{
+    internal class Pravokotnik
+    {
+        private double širina;
+        private double višina;
+        private double ploščina;
+
+        public Pravokotnik()
+        {
+            Širina = 0;
+            Višina = 0;
+        }
+        public Pravokotnik(double š, double v)
+        {
+            Širina = š;
+            Višina = v;
+        }
+        public double Širina { get => širina; set => širina = value; }
+        public double Višina { get => višina; set => višina = value; }
+        public double Ploščina { get => širina * višina; }
+        public static bool operator ==(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploščina == p2.Ploščina;
+        }
+        public static bool operator >(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploščina > p2.Ploščina;
+        }
+        public static bool operator <(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploščina < p2.Ploščina;
+        }
+        public static bool operator >=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploščina >= p2.Ploščina;
+        }
+        public static bool operator <=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploščina <= p2.Ploščina;
+        }
+        public static bool operator !=(Pravokotnik p1, Pravokotnik p2)
+        {
+            return p1.Ploščina != p2.Ploščina;
+        }
+
+        public override string ToString()
+        {
+            return "Višina= " + višina + "\tŠirina= " + širina;
+        }
+
+    }
+}
--- Strukture/Strukture/Program.cs
+++ Strukture/Strukture/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,52 @@ namespace Strukture
             {
                 return "(" + x + "," + y + ", " + z + ")";
             }
+            public Vektor(double x1, double y1,double z1)
+            {
+                x = x1;y=y1;z = z1;
+            }
+            public Vektor(Vektor v)
+            {
+                x = v.x;
+                y = v.y;
+                z = v.z;
+            }
+            public static  Vektor operator+(Vektor levi,Vektor desni)
+            {
+                Vektor r = new Vektor();
+                r.x = levi.x + desni.x;
+                r.y = levi.y + desni.y;
+                r.z = levi.z + desni.z;
+
+                return r;
+            }
+            public static Vektor operator*(double a,Vektor b)
+
+            {
+                Vektor r = new Vektor();
+                r.x = a * b.x;
+                r.y = a * b.y;
+                r.z = a * b.z;
+                return r;
+            }
+            public static Vektor operator*(Vektor a,double b)
+            {
+                return b * a;
+
+            }
+            public static double operator*(Vektor a,Vektor b)
+            {
+                return a.x * b.x + a.y * b.y + a.z * b.z;
+            }
+            public static bool operator==(Vektor a, Vektor b)
+            {
+                return a.x == b.x && a.y == b.y && a.z == b.z;
+
+            }
+            public static bool operator !=(Vektor a, Vektor b)
+            {
+                return !(a == b);
+            }
         }
         static void Main(string[] args)
         {
@@ -29,6 +76,11 @@ namespace Strukture
             a.z = 3;
             Console.WriteLine(v.ToString());
             Console.WriteLine(a.ToString());
+            Vektor c = v + a;
+            Console.WriteLine("Vsota je "+c.ToString());
+            c = 5 * v;
+            Console.WriteLine("Mnozenje s skalarjem "+c.ToString());
+            Console.WriteLine("v==a? "+(v==a));
             Console.ReadLine();
         }
     }
